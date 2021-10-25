d244fe3da2c2b89a961f7acf627f679cd53316c4
SpiritLetsPlays <spiritelectro.kontakt@gmail.com>
2021-10-25 22:10:12 +0200

    V 2.0.5.1

--- assets/js/save.js
+++
@@ -1,7 +0,0 @@
-/*function getResolution() {
-    alert("Your screen resolution is: " + window.screen.width * window.devicePixelRatio + "x" + window.screen.height * window.devicePixelRatio);
-}
-
-$(document).ready(() => {
-    getResolution();
-})*/

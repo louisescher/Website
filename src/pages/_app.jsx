db0fce265e4bbb1cdb24d7a43f987de037000ab4
Spirit <louisescher@proton.me>
2023-08-12 16:04:12 +0200

    Add noise back in

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,6 +3,15 @@ import '@/css/globals.css';
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <div 
+        id="noise" 
+        style={{ 
+          backgroundImage: "url(/img/bg_noise.webp)",
+          backgroundRepeat: "repeat",
+          mixBlendMode: "overlay", 
+          opacity: "0.25" 
+        }} 
+      />
       <Component {...pageProps} />
     </>
   )
